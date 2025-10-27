729fe9047ab2d82e4e2c0c1eec82b93d5fa728d6
Harpreet-kaurr <kaurhap1995@gmail.com>
2025-10-26 23:11:19 -0400

    product model created

---
+++ models/Product.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    picture: {
+      url: {
+        type: String,
+        trim: true,
+      },
+      public_id: {
+        type: String,
+        trim: true,
+      },
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
